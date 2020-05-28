6169acedccd415a11a1423c5524dc9f706543158
Tessa <tessarrr@gmail.com>
2020-05-28 20:23:58 +0300

    Anime grill added if time 6-12

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const weather = require('weather-js'); 
 const key = require('./key.json').key;
-const flags = require('./flags.json').flags;
+const flags = require('./assets/emojis/flags.json').flags;
 
 const bot = new Discord.Client();
 
@@ -21,21 +21,21 @@ bot.on('message', message => {
         }
 
         weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) { 
-            if (result === undefined) {
-                message.channel.send('Weather not available 😅')
+            if (!result.length || err) {
+                message.channel.send(`Weather not available for that place 😅 (${err})`);
                 return;
             }
-
             var current = result[0].current; 
             var location = result[0].location; 
-
             var currCountry = flags.find(f => f.country === location.name.split(", ")[1]);
             var flag = !!currCountry ? String.fromCodePoint(...currCountry.values) : '';
-            
+            var time = new Date();
+            var pic = time.getHours() >= 6 && time.getHours <= 12 ? 'https://data.whicdn.com/images/236177143/original.jpg' : current.imageUrl;
+
             var embed = new Discord.MessageEmbed()
             .setDescription(`${flag} ${location.name} ${flag}`)
             .setAuthor(`Weather for ${current.observationpoint}`) 
-            .setThumbnail(current.imageUrl) 
+            .setThumbnail(pic) 
             .setColor(0x00AE86) 
             .addField('Day', `${current.shortday}`, true)
             .addField('Observation Time', `${current.observationtime}`, true)
