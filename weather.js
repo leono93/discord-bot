7e903c7a9b2a183612a5283e6689ee4ed619a4db
Leon <t7oele00@students.oamk.fi>
2020-05-28 15:07:18 +0300

    weather.js

---
+++ weather.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const weather = require('weather-js'); 
+
+const prefix = '.';  
+
+bot.on('message', message => {
+
+    let msg = message.content.toUpperCase();  
+    let cont = message.content.slice(prefix.length).split(" "); 
+    let args = cont.slice(1); 
+
+
+    if (msg.startsWith(prefix + 'WEATHER')) { 
+       
+        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) { 
+            if (err) message.channel.send(err);
+
+            if (result.length === 0) {
+                message.channel.send('Enter a location asshole') 
+                return; 
+            }
+
+            var current = result[0].current; 
+            var location = result[0].location; 
+
+            const embed = new Discord.MessageEmbed()
+                .setDescription(`**${current.skytext}**`) 
+                .setAuthor(`Weather for ${current.observationpoint}`) 
+                .setThumbnail(current.imageUrl) 
+                .setColor(0x00AE86) 
+                .addField('Timezone',`UTC${location.timezone}`, true) 
+                .addField('Degree Type',location.degreetype, true)
+                .addField('Temperature',`${current.temperature} Degrees`, true)
+                .addField('Feels Like', `${current.feelslike} Degrees`, true)
+                .addField('Winds',current.winddisplay, true)
+                .addField('Humidity', `${current.humidity}%`, true)
+
+                message.channel.send({embed});
+        });
+    }
+
+});
+
+bot.on('ready', () => {
+
+    console.log('Bot started.');
+
+});
+
+bot.login('NzE0NDQ2ODY5OTg3NTkwMTQ0.Xs-dOg.Q_9_1NT2NaUpdYIf2S74RGDqhB8');
